fd9aa692e7426f716ef3d1d9251b225054dabe04
nub24 <nub24@rambler.ru>
2023-07-02 15:42:15 +0700

    clear rep

--- backend/routes/signin.js
+++
@@ -1,7 +0,0 @@
-const signinRouter = require('express').Router();
-const { validationAuth } = require('../middlewares/validation');
-const { login } = require('../controllers/users');
-
-signinRouter.post('/signin', validationAuth, login);
-
-module.exports = signinRouter;
--- backend/routes/signup.js
+++
@@ -1,7 +0,0 @@
-const signupRouter = require('express').Router();
-const { validationCreateUser } = require('../middlewares/validation');
-const { createUser } = require('../controllers/users');
-
-signupRouter.post('/signup', validationCreateUser, createUser);
-
-module.exports = signupRouter;
